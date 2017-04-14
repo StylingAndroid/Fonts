e56cd0304bd3bf5246d94b6c9ce0c65c507ae063
Mark Allison <mark@stylingandroid.com>
2017-04-14 09:06:04 +0100

    Code for Part 2

--- app/src/main/java/com/stylingandroid/o/fonts/MainActivity.java
+++ app/src/main/java/com/stylingandroid/o/fonts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.stylingandroid.o.fonts;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface nunito = getResources().getFont(R.font.nunito);
+        TextView text = (TextView) findViewById(R.id.nunito_programmatic);
+        text.setTypeface(nunito, Typeface.BOLD_ITALIC);
     }
 }
