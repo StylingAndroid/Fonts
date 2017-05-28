7a4f9671db653b34361e4dd427865d16fd9d2823
Mark Allison <mark@stylingandroid.com>
2017-05-28 13:47:55 +0100

    Updates for `revisited` article

--- app/src/main/java/com/stylingandroid/o/fonts/MainActivity.java
+++ app/src/main/java/com/stylingandroid/o/fonts/MainActivity.java
@@ -3,6 +3,7 @@ package com.stylingandroid.o.fonts;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -12,8 +13,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Typeface nunito = getResources().getFont(R.font.nunito);
-        TextView text = (TextView) findViewById(R.id.nunito_programmatic);
+        Typeface pacifico = ResourcesCompat.getFont(this, R.font.pacifico);
+        Typeface nunito = ResourcesCompat.getFont(this, R.font.nunito);
+
+        TextView pacificoText = findViewById(R.id.pacifico);
+        pacificoText.setTypeface(pacifico);
+
+        TextView regular = findViewById(R.id.nunito_regular);
+        regular.setTypeface(nunito, Typeface.NORMAL);
+        TextView bold = findViewById(R.id.nunito_bold);
+        bold.setTypeface(nunito, Typeface.BOLD);
+        TextView italic = findViewById(R.id.nunito_italic);
+        italic.setTypeface(nunito, Typeface.ITALIC);
+        TextView boldItalic = findViewById(R.id.nunito_bold_italic);
+        boldItalic.setTypeface(nunito, Typeface.BOLD_ITALIC);
+
+        TextView text = findViewById(R.id.nunito_programmatic);
         text.setTypeface(nunito, Typeface.BOLD_ITALIC);
     }
 }
