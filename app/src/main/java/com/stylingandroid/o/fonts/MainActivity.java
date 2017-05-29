c043882d774e4f60d604fbb0b084a25355743b04
Mark Allison <mark@stylingandroid.com>
2017-05-29 10:57:50 +0100

    Use AppCompat to get fonts working

--- app/src/main/java/com/stylingandroid/o/fonts/MainActivity.java
+++ app/src/main/java/com/stylingandroid/o/fonts/MainActivity.java
@@ -1,32 +1,19 @@
 package com.stylingandroid.o.fonts;
 
-import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Typeface pacifico = ResourcesCompat.getFont(this, R.font.pacifico);
         Typeface nunito = ResourcesCompat.getFont(this, R.font.nunito);
-
-        TextView pacificoText = (TextView) findViewById(R.id.pacifico);
-        pacificoText.setTypeface(pacifico);
-
-        TextView regular = (TextView) findViewById(R.id.nunito_regular);
-        regular.setTypeface(nunito, Typeface.NORMAL);
-        TextView bold = (TextView) findViewById(R.id.nunito_bold);
-        bold.setTypeface(nunito, Typeface.BOLD);
-        TextView italic = (TextView) findViewById(R.id.nunito_italic);
-        italic.setTypeface(nunito, Typeface.ITALIC);
-        TextView boldItalic = (TextView) findViewById(R.id.nunito_bold_italic);
-        boldItalic.setTypeface(nunito, Typeface.BOLD_ITALIC);
 
         TextView text = (TextView) findViewById(R.id.nunito_programmatic);
         text.setTypeface(nunito, Typeface.BOLD_ITALIC);
